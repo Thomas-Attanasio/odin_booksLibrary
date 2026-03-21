ab04085591a9ad33d745064213007bc5abbde2a9
therendev05 <therendev@gmail.com>
2026-03-21 10:16:59 +0100

    Upgrade the change read status button style

--- script.js
+++ script.js
@@ -15,6 +15,35 @@ hideFormButton.addEventListener('click', () => {
     newBookForm.reset();
 });
 
+function displayBook(book) {
+    const newBook = document.createElement('div');
+    newBook.classList.add('newBookContainer');
+
+    newBook.innerHTML = `
+        <div style="margin-bottom: 10px;"><b>Title:</b> ${book.title}</div>
+        <div style="margin-bottom: 10px;"><b>Author:</b> ${book.author}</div>
+        <div style="margin-bottom: 10px;"><b>Pages:</b> ${book.pagesNumber}</div>
+        <div><b>Read:</b> <span class="readText">${book.readStatus ? 'Yes' : 'No'}</span></div>
+    `;
+
+    const changeStatusButton = document.createElement('button');
+    changeStatusButton.classList.add('changeReadStatus');
+    changeStatusButton.innerHTML = 'Change Read Status';
+
+    changeStatusButton.addEventListener('click', () => {
+        const readStatusText = newBook.querySelector('.readText');
+
+        book.readStatus = !book.readStatus;
+        readStatusText.textContent = book.readStatus ? 'Yes' : 'No';
+    });
+
+    newBooksContainer.appendChild(newBook);
+    newBook.appendChild(changeStatusButton);
+}
+
+const defaultBook = addBook('Harry Potter and the Philosopher\'s Stone', 'J.K. Rowling', 352, true);
+displayBook(defaultBook);
+
 newBookForm.addEventListener('submit', (e) => {
     e.preventDefault();
 
@@ -31,30 +60,7 @@ newBookForm.addEventListener('submit', (e) => {
     }
 
     const currentBook = addBook(title, author, pagesNumber, isRead);
-
-    const newBook = document.createElement('div');
-    newBook.classList.add('newBookContainer');
-
-    newBook.innerHTML = `
-        <div style="margin-bottom: 10px;"><b>Title:</b> ${title}</div>
-        <div style="margin-bottom: 10px;"><b>Author:</b> ${author}</div>
-        <div style="margin-bottom: 10px;"><b>Pages:</b> ${pagesNumber}</div>
-        <div><b>Read:</b> <span class="readText">${isRead ? 'Yes' : 'No'}</span></div>
-    `;
-
-    const changeStatusButton = document.createElement('button');
-    changeStatusButton.classList.add('changeReadStatus');
-    changeStatusButton.innerHTML = 'Change Read Status';
-
-    changeStatusButton.addEventListener('click', () => {
-        const readStatusText = newBook.querySelector('.readText');
-
-        currentBook.readStatus = !currentBook.readStatus;
-        readStatusText.textContent = currentBook.readStatus ? 'Yes' : 'No';
-    });
-
-    newBooksContainer.appendChild(newBook);
-    newBook.appendChild(changeStatusButton);
+    displayBook(currentBook);
 
     booksContainer.style.display = 'none';
     newBookForm.reset();
